27959e44f6e61b97117aa0693753edd574510a54
Andy Gelme <andyg@geekscape.org>
2012-05-28 06:00:39 +1000

    Temporary fix for PIN_ONE_WIRE :(

--- aiko_devices/AikoDeviceTemperatureDS1820.h
+++ aiko_devices/AikoDeviceTemperatureDS1820.h
@@ -1,7 +1,8 @@
 #ifndef AikoDeviceTemperatureDS1820_h
 #define AikoDeviceTemperatureDS1820_h
 
-static const byte PIN_ONE_WIRE = 5;
+// static const byte PIN_ONE_WIRE = 5;
+#define PIN_ONE_WIRE  5
 
 extern byte temperatureDS1820Ready;
 
